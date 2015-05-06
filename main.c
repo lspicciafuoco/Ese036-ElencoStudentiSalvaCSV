df1f042cd4b0c53e1568880c96778b19daf212db
Alessio Picciafuoco <alessio.picciafuoco@gmail.com>
2015-05-06 16:11:52 +0200

    commit

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -19,6 +20,33 @@ struct s_studente {
 typedef struct s_studente studente;
 
 int main(int argc, char** argv) {
+    studente elenco[N];
+    int i, c;
+    FILE *puntaFile;
     
+    puntaFile = fopen(FNAME, "w");
+    if(puntaFile == NULL) {
+        fprintf(stderr,"Errore nell'apertura del file %s\n", FNAME);
+        exit(1);
+    }
+    
+    fprintf(puntaFile, "nome;cognome;eta';classe\n");
+    for(i = 0; i < N; i++) {
+        printf("Inserisci il nome %d : ", i);
+        scanf("%s", elenco[i].nome);
+        printf("Inserisci il cognome %d : ", i);
+        scanf("%s", elenco[i].cognome);
+        printf("Inserisci l'eta' %d : ", i);
+        scanf("%d", &(elenco[i].eta));
+        printf("Inserisci la classe %d : ", i);
+        scanf("%s", elenco[i].classe); 
+        c = fprintf(puntaFile, "%s;%s;%d;%s\n", elenco[i].nome, elenco[i].cognome, elenco[i].eta, elenco[i].classe);
+        if(c < 0) {
+            fprintf(stderr, "Errore di scrittura nel file\n");
+            exit(2);
+        }
+    }
+    fclose(puntaFile);
+    printf("Dati salvati nel file %s\n", FNAME);
     return (EXIT_SUCCESS);
 }
